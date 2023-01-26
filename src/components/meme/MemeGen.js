d204a733fef265848d4e5e6c6d20be923eeb98cd
OlaniyiHope <olaniyihoppee@gmail.com>
2023-01-26 05:30:35 +0100

    removed from meme generator

--- src/components/meme/MemeGen.js
+++ src/components/meme/MemeGen.js
@@ -2,17 +2,8 @@ import { React, useState } from "react";
 import "./meme.css";
 import memedata from "../../memedata";
 const MemeGen = () => {
-  const [memeImage, setmemeImage] = useState();
-
-  const getMemeImage = async () => {
-    const memesArray = memedata.data.memes;
-    const randomNumber = Math.floor(Math.random() * memesArray.length);
-    setmemeImage(memesArray[randomNumber].url);
-  };
-  const nums = [1, 2, 3, 5, 5];
   return (
     <>
-      {nums && nums.map((item) => <p>{item * item}</p>)}
       <div className="meme">
         <h1>MEMES GENERATOR</h1>
         <h4>Create your own memes here</h4>
@@ -23,10 +14,7 @@ const MemeGen = () => {
           <input type="text" placeholder="Bottom Text" className="form-input" />
           <input type="text" placeholder="Bottom Text" className="form-input" />
 
-          <button className="form-button" onClick={getMemeImage}>
-            get a new meme image
-          </button>
-          <img src={memeImage} />
+          <button className="form-button">get a new meme image</button>
         </form>
       </main>
     </>
